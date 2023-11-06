fba34f579ed2e185021a658b94d9bdccf9d38483
Juliana Ramayo <71157416+JulianaRamayo@users.noreply.github.com>
2023-11-06 12:16:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+import OpenAI from 'openai';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const openai = new OpenAI({
+  apiKey: "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" // This is also the default, can be omitted
+});
+//organization: "org-xJYV1N2JPt8hsV9jv0zusxCy"
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+    try {
+        const { messages } = req.body;
+
+        const chatCompletion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { "role": "system", "content": "You are HelloKittyGPT a helpful, friendly and cute assistant that will help with all sorts of tasks"},
+                ...messages
+                // { "role": "user", "content": `${message}` }
+            ],
+        })
+        res.json({
+            completion: chatCompletion.choices[0].message
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while processing your request.' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
